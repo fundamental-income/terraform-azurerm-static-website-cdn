1d852202b6e4c036e9a61694e6a7383779359825
Ian Cornett <32746772+icornett@users.noreply.github.com>
2020-10-02 16:04:53 -0700

    install az cdn module (#6)
    * install az cdn module
    
    * typo in cmdlet

--- Setup-AzCdnCustomDomain.ps1
+++ Setup-AzCdnCustomDomain.ps1
@@ -1,3 +1,4 @@
+Install-Module Az.Cdn -Force
 Import-Module Az.Cdn -Force
 $cdnProfile = Get-AzCdnProfile -ProfileName StaticCdnProfile -ResourceGroupName $env:RG_NAME
 $endpoint = Get-AzCdnEndpoint -ProfileName $cdnProfile.Name -ResourceGroupName $env:RG_NAME
